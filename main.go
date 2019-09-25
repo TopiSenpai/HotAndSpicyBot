584e99877a4e52d4a2f8f388e7cc8758abfadae1
Roman Auer <ra@vioma.de>
2019-09-25 18:10:44 +0200

    some work done

--- main.go
+++ main.go
@@ -15,21 +15,36 @@ var api *slack.Client
 var conf config
 var data save
 
+var daysOfWeek = map[string]time.Weekday{
+	"sun": time.Sunday,
+	"mon": time.Monday,
+	"tue": time.Tuesday,
+	"wed": time.Wednesday,
+	"thu": time.Thursday,
+	"fri": time.Friday,
+	"sat": time.Saturday,
+}
+
+func parseWeekday(v string) (time.Weekday, error) {
+	if len(v) < 3 {
+		return -1, fmt.Errorf("Invalid weekday (%s) to short")
+	}
+	v = strings.ToLower(v[:3])
+	if d, ok := daysOfWeek[v]; ok {
+		return d, nil
+	}
+
+	return -1, fmt.Errorf("invalid weekday '%s'", v)
+}
+
 type config struct {
-	Token     string `json:"token"`
-	ChannelID string `json:"channel_id"`
+	Token      string `json:"token"`
+	GroupID    string `json:"group_id"`
+	CookingDay string `json:"cooking_day"`
 }
 
 type save struct {
-	Users       map[string]user `json:"users"`
-	DishHistory []dishHistory   `json:"dish_history"`
-}
-
-type user struct {
-	SlackID      string    `json:"slack_id"`
-	LastCooked   string    `json:"last_cooked"`
-	LastHelped   string    `json:"last_helped"`
-	UnavailUntil time.Time `json:"unavail_until"`
+	DishHistory []dishHistory `json:"dish_history"`
 }
 
 type dishHistory struct {
@@ -45,7 +60,17 @@ type cooked struct {
 }
 
 func loadFromJSON() error {
-	b, err := ioutil.ReadFile("save.json")
+	b, err := ioutil.ReadFile("config.json")
+	if os.IsNotExist(err) {
+		return fmt.Errorf("Please deposit valid config: %s", err.Error())
+	} else if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(b, &conf); err != nil {
+		return err
+	}
+
+	b, err = ioutil.ReadFile("save.json")
 	if os.IsNotExist(err) {
 		return nil
 	} else if err != nil {
@@ -62,7 +87,7 @@ func saveToJSON() error {
 	if err != nil {
 		return err
 	}
-	if err := ioutil.WriteFile("save.json", b, 666); err != nil {
+	if err := ioutil.WriteFile("save.json", b, 777); err != nil {
 		return err
 	}
 	return nil
@@ -70,14 +95,29 @@ func saveToJSON() error {
 
 func update() {
 	defer saveToJSON()
-	users, err := api.GetUserGroupMembers(conf.ChannelID)
+	if data.DishHistory == nil || len(data.DishHistory) < 1 {
+
+	}
+	fmt.Printf("%#v\n", data.DishHistory)
+	fmt.Println(conf.GroupID)
+	group, err := api.GetGroupInfo(conf.GroupID)
 	if err != nil {
 		panic(err)
 	}
-	for i := range users {
+	for i := range group.Members {
+		fmt.Println(group.Members[i])
+		user, err := api.GetUserInfo(group.Members[i])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 		//save.Users[_] =
-		fmt.Printf("%#v\n", users[i])
+		fmt.Printf("%#v\n", user)
 	}
+}
+
+func newDish() {
+	date := time.Now().Weekday
 }
 
 func main() {
@@ -86,26 +126,37 @@ func main() {
 		panic(err)
 	}
 
-	b, err := ioutil.ReadFile("config.json")
+	api = slack.New(conf.Token)
+	if api == nil {
+		panic("empty api")
+	}
+	resp, err := api.AuthTest()
 	if err != nil {
 		panic(err)
 	}
-	if err := json.Unmarshal(b, &conf); err != nil {
-		panic(err)
+	if resp == nil {
+		panic("Empty auth test response")
 	}
-	api := slack.New(conf.Token)
+
+	prefix := "<@" + resp.UserID + ">"
+
 	fmt.Println("Hot & Spicy Bot starting...")
+
 	rtm := api.NewRTM()
 
 	go rtm.ManageConnection()
+	time.Sleep(time.Second * 5)
+
+	fmt.Println(prefix)
 
 	update()
+
+	panic("Test")
 
 	for msg := range rtm.IncomingEvents {
 		//fmt.Print("Event Received: ")
 		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
-			prefix := "<@" + rtm.GetInfo().User.ID + ">"
 			if !strings.HasPrefix(ev.Msg.Text, prefix) {
 				//fmt.Println("Message NOT for me: '" + ev.Text + "'")
 				continue
@@ -116,7 +167,7 @@ func main() {
 			if strings.Contains(args, "wer kocht") {
 				api.SendMessage(ev.Channel, slack.MsgOptionText("nicht ich", false))
 			} else if strings.Contains(args, "was wird gekocht") {
-				api.SendMessage(ev.Channel, slack.MsgOptionText("for me", false))
+				api.SendMessage(ev.Channel, slack.MsgOptionText("Schniposa", false))
 			} else if strings.Contains(args, "gerichte") {
 				api.SendMessage(ev.Channel, slack.MsgOptionText("for me", false))
 			} else {
@@ -134,5 +185,4 @@ func main() {
 			//fmt.Printf("Unexpected: %v\n", msg.Data)
 		}
 	}
-
 }
